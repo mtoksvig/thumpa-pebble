dbf0f0e2b59586b827b84946313cf60108e6e4ec
Michael Toksvig <mtoksvig@fb.com>
2013-05-13 11:10:13 -0700

    O3 gets me 24 fps

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 export PATH=~/pebble-dev/arm-cs-tools/bin:$PATH
 ./waf configure
 ./waf build # -vvv
-../../../libpebble/p.py load build/thumpa-pebble.pbw
+~/pebble-dev/libpebble/p.py load build/thumpa-pebble.pbw
